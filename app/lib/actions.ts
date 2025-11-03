f75b9bab02c099a3c5c52f0dd1d4abeec9b578eb
leonardo_fonteles <fonteles.leonardo@gmail.com>
2025-11-03 11:20:06 -0300

    improving acessibility the form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,34 +8,58 @@ const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
 const formSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({ invalid_type_error: 'Please select a customer' }),
+    amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater tahn $0.' }),
+    status: z.enum(['pending', 'paid'], { invalid_type_error: 'Please select an invoice status' }),
     date: z.string(),
 });
+// customerId - O Zod já gera um erro se o campo do cliente estiver vazio, pois espera um tipo string. Mas vamos adicionar uma mensagem amigável se o usuário não selecionar um cliente.
+
+// amount - Já que você está coagindo o tipo de valor de string para number, o padrão será zero se a string estiver vazia. Vamos dizer ao Zod que sempre queremos a quantidade maior que 0 com o .gt() função.
+
+// status- O Zod já gera um erro se o campo de status estiver vazio, pois espera "pendente" ou "pago". Vamos também adicionar uma mensagem amigável se o usuário não selecionar um status.
 
 const CreateInvoice = formSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export type State = {
+  errors?: {
+    customerId?: string[],
+    amount?: string[],
+    status?: string[],
+  };
+  message?: string | null;
+};
 
-    const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+
+    // validate with zod
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Create Invoice.',
+      }
+    }
+
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
     try {
       await sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-          VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
       `;
     } catch (error) {
-      console.log(error);
       return { message: 'Database Error: Failed to Create Invoice' }
     }
-
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 };
@@ -44,14 +68,22 @@ export async function createInvoice(formData: FormData) {
 
 const UpdateInvoice = formSchema.omit({ id: true, date: true });
  
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
     
-  const { customerId, amount, status } = UpdateInvoice.parse({
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
+
+  if(!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    }
+  }
+  
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
  
   try {
